109b8c7e1cc8a43404ec2bc46aab00143108b64d
ieqbl <amnwie@gmail.com>
2025-08-17 12:51:37 +0330

    Initial commit

--- gateway/src/gateway/Main.java
+++ gateway/src/gateway/Main.java
@@ -13,60 +13,47 @@ public class Main
     {
         try
         {
-            // ساخت درایور و گیت‌وی پیامک
             SmsDriver driver = DriverFactory.createDriver();
             SmsGateway gateway = new SmsGateway(driver);
 
             String number = "09120000000";
             String message = "Test with Module";
 
-            // ارسال پیام
             gateway.send(number, message);
 
-            // اتصال به پایگاه داده
             DbServices db = new DbServices();
             db.Connect();
 
-            // گرفتن ریپازیتوری‌ها
             ConfigRepository Cr = db.getConfigRepository();
             SmsLogRepository Sr = db.getSmsLogRepository();
 
-            // ایجاد کانفیگ‌ها
-            Cr.save(new Config(23, "Twilio"));
-            Cr.save(new Config(313, "Nexmo"));
+            Cr.save(new Config(1, "Twilio"));
+            Cr.save(new Config(2, "Nexmo"));
 
-            // خواندن همه کانفیگ‌ها
             System.out.println("Config List");
             Cr.findAll().forEach(c ->
                     System.out.println(c.GetID() + ": " + c.GetConfigDriver())
             );
 
-            // خواندن یک کانفیگ خاص
             Config config = Cr.findByID(1);
             if (config != null)
             {
                 System.out.println("findByID(1): " + config.GetConfigDriver());
 
-                // بروزرسانی
                 config.SetConfigDriver("Updated Driver");
                 Cr.update(config);
             }
 
-            // حذف یک کانفیگ خاص
             Cr.delete(2);
 
-            // حذف همه کانفیگ‌ها
             Cr.deleteAll();
 
-            // اجرای مایگریشن (ساخت جدول‌ها)
             MigrationRunner migrator = new MigrationRunner(db.getConnection());
             migrator.run();
 
-            // ذخیره لاگ پیامک
             SmsLog log = new SmsLog("System", number, driver.getClass().getSimpleName(), "Sent successfully");
             Sr.save(log);
 
-            // بستن ارتباط با پایگاه‌داده
             db.close();
         }
         catch (Exception e)
--- gateway/src/module-info.java
+++ gateway/src/module-info.java
@@ -1,6 +1,12 @@
-module gateway
-{
+module gateway {
     requires sms.api;
     requires java.sql;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.web;
+
     uses api.SmsDriver;
+
+    opens com.example.gateway to spring.core, spring.beans, spring.context;
 }
