1a59dc2ea7b8075fcf8693e2911aad1df06fd43f
ieqbl <amnwie@gmail.com>
2025-08-19 14:22:00 +0330

    Initial commit

--- gateway/src/gateway/model/SmsLog.java
+++ gateway/src/gateway/model/SmsLog.java
@@ -58,3 +58,4 @@ public class SmsLog
     public String getDriverResponse() { return driverResponse; }
     public void setDriverResponse(String driverResponse) { this.driverResponse = driverResponse; }
 }
+
