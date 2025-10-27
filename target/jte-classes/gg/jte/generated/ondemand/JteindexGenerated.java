9570c745b8a72522f0fce5abde61e012128d0d4e
ieqbl <amnwie@gmail.com>
2025-10-27 12:18:34 +0330

    JTE rendering

---
+++ target/jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
@@ -0,0 +1,12 @@
+package gg.jte.generated.ondemand;
+@SuppressWarnings("unchecked")
+public final class JteindexGenerated {
+	public static final String JTE_NAME = "index.jte";
+	public static final int[] JTE_LINE_INFO = {1,1,1,1,1,1,1,1,1,1,1,1};
+	public static void render(gg.jte.TemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		jteOutput.writeContent("Hello @name!\r\n");
+	}
+	public static void renderMap(gg.jte.TemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}
