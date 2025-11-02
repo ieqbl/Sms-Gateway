a038748f1a3ea727e0e1405e8ecdcbfe633cf8cb
ieqbl <amnwie@gmail.com>
2025-11-02 20:08:06 +0330

    add database in JTE

--- gateway/src/gateway/database/Database.java
+++ gateway/src/gateway/database/Database.java
@@ -1,4 +1,37 @@
 package gateway.database;
 
-public class Database {
+
+import java.sql.*;
+import java.sql.SQLException;
+import java.util.*;
+
+public class Database
+{
+    public static List<Message> getMessages()
+    {
+        List<Message> messages = new ArrayList<>();
+        String sql ="SELECT id, phone, content FROM messages ORDER BY DESC";
+        try (Connection connection = DriverManager.getConnection(DatebaseConfig.URL,DatebaseConfig.USER,DatebaseConfig.PASSWORD);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql))
+        {
+            while (resultSet.next())
+            {
+                messages.add(new Message(
+                        resultSet.getInt("id"),
+                        resultSet.getString("phone"),
+                        resultSet.getString("content")));
+
+            }
+            catch (SQLException e)
+            {
+                e.printStacktrace();
+            }
+
+            return messages;
+
+        }
+
+    }
+
 }
--- gateway/src/gateway/database/DatebaseConfig.java
+++ gateway/src/gateway/database/DatebaseConfig.java
@@ -1,4 +1,8 @@
 package gateway.database;
 
-public class DatebaseConfig {
+public class DatebaseConfig
+{
+    public static final String URL = "jdbc:postgresql://localhost:5432/smsgateway";
+    public static final String USER = "Ieqbli";
+    public static final String PASSWORD = "Mamali@2005";
 }
